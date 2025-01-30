2fb5d100ab5c99937ff93cc9be1b4f1167b38080
Lorrant Marques <lorrant2000@gmail.com>
2025-01-30 11:09:13 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Caffiend ☕
+
+A coffee consumption tracker with user authentication and estimated caffeine bloodstream persistence.
+
+🔗 Try project: [](#)
+
+📚 The project was built following [Smoljames'](#https://www.youtube.com/@Smoljames) course: <br />[ReactJS Full Course | Build & Deploy 3 Modern Web Apps and Get Hired!](#https://www.youtube.com/watch?v=iKpkVKubvKk&t=8176s)
+
+# Technologies
+1. React.JS
+2. Firebase
+3. CSS
+4. HTML
---
+++ src/components/Layout.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react"
+import Modal from "./Modal"
+import Authentication from "./Authentication"
+import { useAuth } from "../context/AuthContext"
+
+export default function Layout(props) {
+    const { children } = props
+
+    const [showModal, setShowModal] = useState(false)
+
+    const {globalUser, logout} = useAuth()
+
+    const header = (
+        <header>
+            <div>
+                <h1 className="text-gradient">CAFFIEND</h1>
+                <p>For Coffee Insatiates</p>
+            </div>
+
+            {globalUser ? (
+                <button onClick={logout}>
+                    <p>Log out</p>
+                    <i className="fa-solid fa-mug-saucer"></i>
+                </button>
+                ) : (
+                <button onClick={() => {setShowModal(true)}}>
+                    <p>Sign up free</p>
+                    <i className="fa-solid fa-mug-hot"></i>
+                </button>
+                )
+            }
+        </header>
+    )
+    
+    const footer = (
+        <footer>
+            <p><span className="text-gradient">Caffiend</span> was made by <a href="https://www.smoljames.com" target="_blank">Smoljames</a> <br /> using the <a href="https://www.fantacss.smoljames.com" target="_blank">FantaCSS</a> design library.<br />Check out the project on <a target="_blank" href="">GitHub</a></p>
+        </footer>
+    )
+
+    function handleCloseModal() {
+        setShowModal(false)
+    }
+
+    return (
+        <>
+            {showModal && (
+                <Modal handleCloseModal={handleCloseModal}>
+                    <Authentication handleCloseModal={handleCloseModal}/>
+                </Modal>
+            )}
+
+            {header}
+            <main>
+                {children}
+            </main>
+            {footer}
+        </>
+    )
+}
